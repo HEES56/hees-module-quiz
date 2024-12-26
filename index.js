b0fb771e90365ccbb27db7261d92672b5572167e
HEES56 <rlagmltjd125@gmail.com>
2024-12-26 16:50:15 +0900

    add : quiz

---
+++ index.js
@@ -0,0 +1,47 @@
+import { validateUser, validateEmail } from "./validation";
+import { assignUserGrade, formatUserData } from "./serialization";
+import { serializeUser,} from "./transformation";
+
+const processUserData = (user) => {
+  try {
+  validateUser(user);
+  validateEmail(user);
+
+  const gradUser = assignUserGrade(user);
+  const formatUser = formatUserData(gradUser);
+  const serialUser = serializeUser(formatUser);
+  return serialUser;
+  } catch(err) {
+    console.log('err', err);
+  }
+};
+
+const testCase1 = {
+  name: "강태진",
+  email: "taejin@taejin.com",
+  age: 25
+};
+
+const testCase2 = {
+  name: "신진호",
+  email: "jinho@jinho.com",
+  age: 15
+};
+
+const testCase3 = {
+  name: "이동혁",
+  email: "hyuk@hyuk.com",
+  age: 45
+};
+
+const result1 = processUserData(testCase1);
+console.log(result1);
+// 출력: "강태진,taejin@taejin.com,25,ADULT,2024-12-26"
+
+const result2 = processUserData(testCase2);
+console.log(result2);
+// 출력: "신진호,jinho@jinho.com,15,JUNIOR,2024-12-26"
+
+const result3 = processUserData(testCase3);
+console.log(result3);
+// 출력: "이동혁,hyuk@hyuk.com,45,SENIOR,2024-12-26"
